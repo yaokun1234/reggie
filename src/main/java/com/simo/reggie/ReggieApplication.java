12d36ca7b0fa4af399ec6139c5009379022e32ff
simo <2921847833@qq.com>
2022-05-21 16:29:47 +0800

    使用Sharding-jdbc完成分库

--- src/main/java/com/simo/reggie/ReggieApplication.java
+++ src/main/java/com/simo/reggie/ReggieApplication.java
@@ -16,4 +16,5 @@ public class ReggieApplication {
         SpringApplication.run(ReggieApplication.class,args);
         log.info("项目启动成功");
     }
+
 }
---
+++ src/main/java/com/simo/reggie/config/DataSourceHealthConfig.java
@@ -0,0 +1,30 @@
+package com.simo.reggie.config;
+
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.actuate.autoconfigure.jdbc.DataSourceHealthContributorAutoConfiguration;
+import org.springframework.boot.actuate.health.AbstractHealthIndicator;
+import org.springframework.boot.actuate.jdbc.DataSourceHealthIndicator;
+import org.springframework.boot.jdbc.metadata.DataSourcePoolMetadataProvider;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+
+import javax.sql.DataSource;
+import java.util.Map;
+
+@Configuration
+public class DataSourceHealthConfig extends DataSourceHealthContributorAutoConfiguration {
+
+    public DataSourceHealthConfig(Map<String, DataSource> dataSources, ObjectProvider<DataSourcePoolMetadataProvider> metadataProviders) {
+        super(dataSources, metadataProviders);
+    }
+
+    @Override
+    protected AbstractHealthIndicator createIndicator(DataSource source) {
+        DataSourceHealthIndicator indicator = (DataSourceHealthIndicator) super.createIndicator(source);
+        if(!StringUtils.hasText(indicator.getQuery())){
+            indicator.setQuery("select 1");
+        }
+        return indicator;
+    }
+}
+
